e260fb2904fa06ba7020a216a83c9c44e792faa2
Lava-Java <51033538+Lava-Java@users.noreply.github.com>
2019-12-06 09:49:01 -0500

    created user sign in, sign up, cart and user sagas

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -7,7 +7,7 @@ export const toggleCartHidden = () => ({
 export const addItem = item => ({
     type: CartActionTypes.ADD_ITEM,
     payload: item
-})
+});
 
 export const removeItem = item => ({
     type: CartActionTypes.REMOVE_ITEM,
@@ -17,4 +17,8 @@ export const removeItem = item => ({
 export const clearItemFromCart = item => ({
     type: CartActionTypes.CLEAR_ITEM_FROM_CART,
     payload: item
-})
+});
+
+export const clearCart = () => ({
+    type: CartActionTypes.CLEAR_CART
+});
