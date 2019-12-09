045d7ffae4cfc1ba3f9050cede1476d3cda964fc
Lava-Java <51033538+Lava-Java@users.noreply.github.com>
2019-12-09 14:07:31 -0500

    divided create react app into client server, and created express server to handle backend Stripe payment processing

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_eP3rts9uyFngEfv71cfhUc3p00a4CeTYoU';
+
+    const onToken = token => {
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment Successful!')
+        }).catch(error => {
+            console.log("Payment Error: ", JSON.parse(error));
+            alert("There was an error with your payment. Please use the provided credit card information.")
+        }) 
+        
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='Blessed Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://sendeyo.com/up/d/f3eb2117da'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
